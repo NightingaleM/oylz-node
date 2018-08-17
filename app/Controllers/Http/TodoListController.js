5c5d312e78e848c961d02d4c1de145c43b9920c9
欧阳润驰 <oy_runchi@qq.com>
2018-08-17 17:50:36 +0800

    x

--- app/Controllers/Http/TodoListController.js
+++ app/Controllers/Http/TodoListController.js
@@ -1,11 +1,61 @@
 'use strict'
 const Tag = use('App/Models/Tag')
 const TodoList = use('App/Models/TodoList')
+const Database = use('Database')
 class TodoListController {
-  async index() {
-    const time = request.input('time')
-    // const
-    const yearMsg = TodoList.findAll().where('planningTime','<',time.year)
+  async index({
+    request,
+    response
+  }) {
+    const time = request.only(['year', 'month', 'date'])
+    const nextDay = (() => {
+      let t = `${time.year}-${time.month}-${time.date}`
+      let dayTime = new Date(new Date(t).getTime() + 86400000)
+      return `${dayTime.getFullYear()}-${dayTime.getMonth()+1}-${dayTime.getDate()}`
+    })()
+    const year = {
+      now: `${time.year}-01-01`,
+      next: `${time.year+1}-01-01`,
+    }
+    const month = {
+      now: `${time.year}-${time.month}-01`,
+      next: `${time.month==='12'?parseInt(time.year)+1:time.year}-${time.month==='12'?'01':parseInt(time.month)+1}-01`,
+    }
+
+    const date = {
+      now: `${time.year}-${time.month}-${time.date}`,
+      next: nextDay,
+    }
+
+    const yearMsg = await TodoList.query().with('tags')
+      .where(function () {
+        this.where('type', 'Year')
+          .andWhere('planningTime', '>', year.now)
+          .andWhere('planningTime', '<', year.next)
+          .orWhere('planningTime', '=', year.now)
+      }).fetch()
+    const monthMsg = await TodoList.query().with('tags')
+      .where(function () {
+        this.where('type', 'Month')
+          .andWhere('planningTime', '>', month.now)
+          .andWhere('planningTime', '<', month.next)
+          .orWhere('planningTime', '=', month.now)
+      }).fetch()
+    const dateMsg = await TodoList.query().with('tags')
+      .where(function () {
+        this.where('type', 'Day')
+          .andWhere('planningTime', '>', date.now)
+          .andWhere('planningTime', '<', date.next)
+          .orWhere('planningTime', '=', date.now)
+      }).fetch()
+    response.json({
+      message: 'success',
+      result: {
+        year: yearMsg,
+        month: monthMsg,
+        date: dateMsg
+      }
+    })
   }
 
   async store({
@@ -36,10 +86,22 @@ class TodoListController {
 
   }
 
-  update({
-    params
+  async update({
+    params,
+    response
   }) {
-    return `更新单个资源的请求 ${params.id}`
+    let now = new Date()
+    // return `更新单个资源的请求 ${params.id}`
+    let time = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}-${now.getHours()}:${now.getMinutes()}:10`
+
+    await TodoList.query().where('id', params.id).update({
+      isFinished: 1,
+      finishTime: time
+    })
+    response.json({
+      message: 'finished',
+      result: null
+    })
   }
 
   destroy({
